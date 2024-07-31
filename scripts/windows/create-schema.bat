886c6e76e5e4a98ca37d5a71ee6cc21bcd981640
Michael Vaccaro <mikevac@protonmail.com>
2024-07-30 21:05:23 -0400

    migration to liquibase

--- scripts/windows/create-schema.bat
+++ scripts/windows/create-schema.bat
@@ -5,5 +5,3 @@ SET PGPASSWORD=redrover
 psql -U postgres -f schema\remove-image-bank.sql 
 psql -U postgres -f schema\create-database.sql
 
-SET PGPASSWORD=ibankadmin
-psql -U ibankadmin -f schema\ibank-ddl.sql  image_bank
--- scripts/windows/remove-schema.bat
+++ scripts/windows/remove-schema.bat
@@ -3,4 +3,3 @@
 SET PGPASSWORD=redrover
 
 psql -U postgres -f schema\remove-image-bank.sql 
-rem psql -U postgres -c 'DROP ROLE ibankuser;' -c 'DROP ROLE ibankadmin;'
